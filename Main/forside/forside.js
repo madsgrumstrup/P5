ba618f809ac06c1c17ed62b853f3754bb9f8a064
frejaj <frej1576@edu.ucl.dk>
2020-04-06 22:21:41 +0200

    startknap

--- Main/forside/forside.js
+++ Main/forside/forside.js
@@ -8,8 +8,9 @@ var closeBtn = document.getElementsByClassName('closeBtn')[0];
 // Listen for open click
 slutBtn.addEventListener('click', openModal);
 // Listen for close click
-startBtn.addEventListener('click', closeModal);
+closeBtn.addEventListener('click', closeModal);
 // Listen for outside click
+startBtn.addEventListener('click', closeModal);
 window.addEventListener('click', outsideClick);
 
 // Function to open modal
@@ -17,6 +18,15 @@ function openModal(){
   modal.style.display = 'block';
 }
 
+// Function to close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
 
+// Function to close modal if outside click
+function outsideClick(e){
+  if(e.target == modal){
+  modal.style.display = 'none';
+  }
 
-// Function to close modal if cklick on button
+}
