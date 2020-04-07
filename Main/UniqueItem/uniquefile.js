112f5a66e464d48f75b030c9f80101ebf72a4ed2
Mikkermand <Mikkermand@users.github.com>
2020-04-07 10:42:10 +0200

    Javascript til pop op hint

---
+++ Main/UniqueItem/uniquefile.js
@@ -0,0 +1,36 @@
+const openModalButton = document.querySelectorAll('[data-modal-target]')
+const closeModalButton = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButton.forEach(button => {
+  button.addEventListener('click',() => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.aktiv')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButton.forEach(button => {
+  button.addEventListener('click',() => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('aktiv')
+  overlay.classList.add('aktiv')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('aktiv')
+  overlay.classList.remove('aktiv')
+}
