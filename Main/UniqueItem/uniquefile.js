96fe04d9e158276d3a80fc5acda6d80e947963d1
Mikkermand <Mikkermand@users.github.com>
2020-04-08 15:02:38 +0200

    Add items del add

--- Main/UniqueItem/uniquefile.js
+++ Main/UniqueItem/uniquefile.js
@@ -1,6 +1,10 @@
 const openModalButton = document.querySelectorAll('[data-modal-target]')
-const closeModalButton = document.querySelectorAll('[data-close-button]')
+const closeButton = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
+const openHestButton = document.querySelectorAll('[data-hest-target]')
+var antal = 1;
+var antalEl = document.getElementById('antal');
+
 
 openModalButton.forEach(button => {
   button.addEventListener('click',() => {
@@ -26,4 +30,43 @@ function closeModal(modal) {
   if (modal == null) return
   modal.classList.remove('aktiv')
   overlay.classList.remove('aktiv')
+}
+
+
+openHestButton.forEach(button => {
+  button.addEventListener('click',() => {
+    const hest = document.querySelector(button.dataset.hestTarget)
+    openHest(hest)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const hest = document.querySelectorAll('.hest.aktiv')
+  hest.forEach(hest => {
+    closeHest(hest)
+  })
+})
+
+function openHest(hest) {
+  if (hest == null) return
+  hest.classList.add('aktiv')
+  overlay.classList.add('aktiv')
+}
+
+function closeHest(hest) {
+  if (hest == null) return
+  hest.classList.remove('aktiv')
+  overlay.classList.remove('aktiv')
+}
+
+
+function plus() {
+  antal++;
+  antalEl.value = antal;
+}
+
+function minus() {
+  if (antal > 1)
+  antal--;
+  antalEl.value = antal;
 }
