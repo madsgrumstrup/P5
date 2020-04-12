4beaecf8c525a79cb9ecc2b990918911aa3cd2c9
madsgrumstrup <58466272+madsgrumstrup@users.noreply.github.com>
2020-04-12 21:24:26 +0200

    addedtres animations

--- Main/JS/treasures.js
+++ Main/JS/treasures.js
@@ -1,1 +1,12 @@
-// JavaScript Document
+// JavaScript Document 
+//const bubList = document.getElementById("bubbles");
+
+
+//new bubble
+
+//let bubListItem = document.createElement('li');
+//bubListItem.getElementById = 'addmore';
+
+
+
+//bubList.prepend(bubListItem);
