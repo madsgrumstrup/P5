34bb0d1af20d959c1e3f5a179318db72fac2771d
madsgrumstrup <58466272+madsgrumstrup@users.noreply.github.com>
2020-04-12 22:03:40 +0200

    js antal vare added

--- Main/JS/treasures.js
+++ Main/JS/treasures.js
@@ -1,7 +1,9 @@
 // JavaScript Document 
 //const bubList = document.getElementById("bubbles");
 
-
+function scrollBottom() {window.scrollBy(0, 999999);}
+if (document.addEventListener) document.addEventListener("DOMContentLoaded", scrollBottom, false)
+else if (window.attachEvent) window.attachEvent("onload", scrollBottom);
 //new bubble
 
 //let bubListItem = document.createElement('li');
